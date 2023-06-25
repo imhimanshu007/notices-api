368ffdb7fe51a32f6629075157e9ef1ba07fa2c0
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-06-25 14:34:13 +0530

    added hibernate validator and added validations on dtos

--- src/main/java/com/relaxcoder/noticesapi/dtos/NoticeDto.java
+++ src/main/java/com/relaxcoder/noticesapi/dtos/NoticeDto.java
@@ -1,14 +1,24 @@
 package com.relaxcoder.noticesapi.dtos;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class NoticeDto {
     private Long id;
 
+    @NotEmpty
+    @Size(min = 8, message = "Title Should Have At Least 8 Characters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 16, message = "Description Should Have At Least 8 Characters")
     private String description;
+
+    @NotEmpty
     private String content;
 
 }
--- src/main/java/com/relaxcoder/noticesapi/dtos/RegisterDto.java
+++ src/main/java/com/relaxcoder/noticesapi/dtos/RegisterDto.java
@@ -1,6 +1,10 @@
 package com.relaxcoder.noticesapi.dtos;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +15,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterDto {
+
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String username;
+    @Email
     private String email;
+    @Size(min = 8, message = "Password should be at least 8 Characters Long")
     private String password;
 }
