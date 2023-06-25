6b455731b2b942985aea9d0cb5832d6fb7fb954c
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-06-25 13:18:39 +0530

    initial commit initialized project

---
+++ src/main/java/com/relaxcoder/noticesapi/NoticesApiApplication.java
@@ -0,0 +1,13 @@
+package com.relaxcoder.noticesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/relaxcoder/noticesapi/NoticesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relaxcoder.noticesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
